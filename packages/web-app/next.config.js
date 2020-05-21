e8a54782cac6c75af3c159138e8c3c15020fcf13
Jean Helou <jhe@codamens.fr>
2020-05-21 18:50:08 +0200

    (KO) using webpacks resolve aliases

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -1,5 +1,16 @@
 // Tell webpack to compile the "bar" package, necessary if you're using the export statement for example
 // https://www.npmjs.com/package/next-transpile-modules
-const withTM = require('next-transpile-modules')(['bar','baz'])
+const withTM = require("next-transpile-modules")(["bar", "baz"]);
 
-module.exports = withTM()
+const nextConfig = {
+  webpack: function (config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      baz: "../baz/src",
+    };
+
+    return config;
+  },
+};
+
+module.exports = withTM(nextConfig);
