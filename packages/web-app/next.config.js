ad0b3423b59886362fac1e6cf0f6af4dec2895b1
Jean Helou <jhe@codamens.fr>
2020-05-26 18:00:02 +0200

    (OK) replaces manual webpack aliases with tsconfig-paths-webpack-plugin

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -2,25 +2,14 @@
 // https://www.npmjs.com/package/next-transpile-modules
 const withTM = require("next-transpile-modules")(["bar"]);
 
-const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 const nextConfig = {
   webpack: function (config) {
-    /*
-     Optionnaly, adding the following to modules
-     will resolve modules using relative paths instead of
-     going through `<rootDir>/node_modules`
-    */
-    /*
-    config.resolve.modules = [
-      path.join(__dirname, ".."),
-      ...config.resolve.modules,
+    config.resolve.plugins = [
+      ...config.resolve.plugins,
+      new TsconfigPathsPlugin(),
     ];
-    */
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      baz: "baz/src",
-    };
 
     return config;
   },
