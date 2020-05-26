ca6fcdbbbb905c48211fa6207fec451d1f3a939b
Jean Helou <jhe@codamens.fr>
2020-05-26 17:48:47 +0200

    Revert "(KO) use transpile module to further resolve baz"
    This reverts commit 4125acae694f185e755c3eadfb864cfbb6b1b637.

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -1,6 +1,6 @@
 // Tell webpack to compile the "bar" package, necessary if you're using the export statement for example
 // https://www.npmjs.com/package/next-transpile-modules
-const withTM = require("next-transpile-modules")(["bar", "baz"]);
+const withTM = require("next-transpile-modules")(["bar"]);
 
 const nextConfig = {
   webpack: function (config) {
