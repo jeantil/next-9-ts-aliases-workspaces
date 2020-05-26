047c6850c618172a7593b340d4f48b266d8f56e3
Jean Helou <jhe@codamens.fr>
2020-05-26 17:52:55 +0200

    (OK) fixes webpack alias to be relative to node_modules

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -2,11 +2,24 @@
 // https://www.npmjs.com/package/next-transpile-modules
 const withTM = require("next-transpile-modules")(["bar"]);
 
+const path = require("path");
+
 const nextConfig = {
   webpack: function (config) {
+    /*
+     Optionnaly, adding the following to modules
+     will resolve modules using relative paths instead of
+     going through `<rootDir>/node_modules`
+    */
+    /*
+    config.resolve.modules = [
+      path.join(__dirname, ".."),
+      ...config.resolve.modules,
+    ];
+    */
     config.resolve.alias = {
       ...config.resolve.alias,
-      baz: "../baz/src",
+      baz: "baz/src",
     };
 
     return config;
